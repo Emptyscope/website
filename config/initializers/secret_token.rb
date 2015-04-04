d7e07e6db1473dadd3d5b01024b6390e78207dfe
Brad Allen <me@bradallen.me>
2015-04-04 13:15:43 -0500

    switching to environment variables

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Emptyscope::Application.config.secret_key_base = 'ebc223228e11d8eb9c2d04b632b416f0b64a9e2b2690d5fa99d0b5d8cf7bc8a1e0ffe9fdb48263a6cd3a022ae42eff5775dd14ad271657a3295d1e224e0d6e39'
+Emptyscope::Application.config.secret_key_base = ENV["SECRET_KEY_BASE"]
